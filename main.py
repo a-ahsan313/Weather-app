9a164ff10ef8d33d9002053d7940deb9770302ee
a-ahsan313 <ahsan.aftab313@gmail.com>
2025-07-01 15:23:32 +0500

    Updated code file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 city = input("Enter the name of the city: ")
 
-result = requests.get(url=f'https://api.weatherapi.com/v1/current.json?key=4513db5a69ba4b698b2113954252106&q={city}')
+result = requests.get(url=f'https://api.weatherapi.com/v1/current.json?key=YOUR API={city}')
 
 # print(result.text)
 weather_dic = json.loads(result.text)
